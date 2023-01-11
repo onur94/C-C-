a43430b0dcbf39dc29839f9cda30a5263f68ff4f
Debug <onuur94@hotmail.com>
2023-01-11 05:08:47 +0000

    C

---
+++ floating_point.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x = 3.14;
+    cout << "3.14 -> 0x" << hex << *((int *)&x) << endl;
+
+    int y = 0x4048f5c3;
+    cout << "0x0x4048f5c3 -> " << dec << *((float *)&y) << endl;
+    return 0;
+}
